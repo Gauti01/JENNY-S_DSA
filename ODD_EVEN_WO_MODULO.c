dafb234bdae23ce7e2656a38f2e5432ca06be7c7
GAUTAM SAGAR <hawkins1827stephen@gmail.com>
2022-04-02 17:06:40 +0530

    Update ODD_EVEN_WO_MODULO.c

--- ODD_EVEN_WO_MODULO.c
+++ ODD_EVEN_WO_MODULO.c
@@ -5,11 +5,12 @@ int main()
     printf("Enter the number you want to check ");
     scanf("%d",&n);
    // if( n & 0x01)
-    if((n/2)*2 ==n)
-    printf("Odd");
-    else
+    if(((n/2)*2) ==n)
     printf("Even");
+    else
+    printf("Odd");
     
     return 0;
     
 }
+
